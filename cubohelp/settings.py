f426c300bf7c21887bb8cecc2c71a694feae47d3
Anfradime <anfradime@hotmail.com>
2025-03-06 02:45:05 -0500

    Sistema de usuarios creados

--- cubohelp/settings.py
+++ cubohelp/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'helpdesk', 
+    'helpdesk',  # Asegúrate de agregar tu aplicación
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'cubohelp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "helpdesk/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,6 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- helpdesk/views.py
+++ helpdesk/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import render, redirect
 
-# Create your views here.
+# Registro de usuario
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home")  # Redirigir a la página principal
+    else:
+        form = UserCreationForm()
+    return render(request, "registration/register.html", {"form": form})
+
+# Inicio de sesión
+def user_login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("home")  # Redirigir después de iniciar sesión
+    else:
+        form = AuthenticationForm()
+    return render(request, "registration/login.html", {"form": form})
+
+def home(request):
+    return render(request, 'helpdesk/home.html')
+
+# Cierre de sesión
+def user_logout(request):
+    logout(request)
+    return redirect("home")  # Redirigir a la página principal
+
